c177ea267aa7bb04b49afc09a7296af95cc71b55
arne six <six.arne@gmail.com>
2022-10-25 16:30:47 +0200

    Dunno what but it works

--- source/Scene.cpp
+++ source/Scene.cpp
@@ -359,7 +359,7 @@ namespace dae {
 		m_Camera.fovAngle = 45.0f;
 
 		const auto matLambert_GrayBlue = AddMaterial(new Material_Lambert({ .49f, 0.57f, 0.57f }, 1.f));
-		const auto matLambert_White = AddMaterial(new Material_Lambert(colors::White, 1.f));
+		const auto matLambert_Gray = AddMaterial(new Material_Lambert(colors::Gray, 1.f));
 
 		//Plane
 		AddPlane({ -5.f, 0.f, 0.f }, { 1.f, 0.f,0.f }, matLambert_GrayBlue);
@@ -368,7 +368,7 @@ namespace dae {
 		AddPlane({ 0.f, 10.f, 0.f }, { 0.f, -1.f,0.f }, matLambert_GrayBlue);
 		AddPlane({ 0.f, 0.f, 10.f }, { 0.f, 0.f,-1.f }, matLambert_GrayBlue);
 
-		pMesh = AddTriangleMesh(TriangleCullMode::BackFaceCulling, matLambert_White);
+		pMesh = AddTriangleMesh(TriangleCullMode::BackFaceCulling, matLambert_Gray);
 		Utils::ParseOBJ("Resources/lowpoly_bunny2.obj",
 			pMesh->positions,
 			pMesh->normals,
